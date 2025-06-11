99d33a35a6eebe5a3b28462af9094a6047beadcf
Felipe <felipe.mreinoso@outlook.com>
2025-06-11 11:50:08 -0300

    Adicionando a tela de Login

--- ExemploAula/Form1.cs
+++ ExemploAula/Form1.cs
@@ -11,5 +11,25 @@ namespace ExemploAula
         {
 
         }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if(txt_usuario.Text == "Admin" && txt_senha.Text == "123456")
+            {
+                MessageBox.Show("Usuário Logado Com Sucesso");
+                txt_usuario.Clear();
+                txt_senha.Clear();
+
+                Hide();
+                frm_menu menu = new frm_menu();
+                menu.Show();
+            }
+            else
+            {
+                MessageBox.Show("Usuário/senha invalidos");
+                txt_usuario.Clear();
+                txt_senha.Clear();
+            }
+        }
     }
 }
