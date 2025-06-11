2863c05a239b89048e83ddc2a7b11513550b8ef2
Felipe <felipe.mreinoso@outlook.com>
2025-06-11 10:40:29 -0300

    Adicionando o usuario

--- ExemploAula/Form1.Designer.cs
+++ ExemploAula/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lbl_login = new Label();
+            label1 = new Label();
+            lbl_usuario = new Label();
+            SuspendLayout();
+            // 
+            // lbl_login
+            // 
+            lbl_login.AutoSize = true;
+            lbl_login.Font = new Font("Arial", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl_login.Location = new Point(323, 9);
+            lbl_login.Name = "lbl_login";
+            lbl_login.Size = new Size(110, 41);
+            lbl_login.TabIndex = 1;
+            lbl_login.Text = "Login";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(108, 102);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 2;
+            // 
+            // lbl_usuario
+            // 
+            lbl_usuario.AutoSize = true;
+            lbl_usuario.Font = new Font("Arial", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl_usuario.Location = new Point(197, 102);
+            lbl_usuario.Name = "lbl_usuario";
+            lbl_usuario.Size = new Size(74, 22);
+            lbl_usuario.TabIndex = 4;
+            lbl_usuario.Text = "Usuário";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lbl_usuario);
+            Controls.Add(label1);
+            Controls.Add(lbl_login);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbl_login;
+        private Label label1;
+        private Label lbl_usuario;
     }
 }
